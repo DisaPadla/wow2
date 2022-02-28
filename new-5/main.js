694a94372e5477ce172c38708a40a233bd352bdf
Denys <denys@4594.local>
2022-02-28 21:49:36 +0200

    fix

--- new-5/main.js
+++ new-5/main.js
@@ -1,7 +1,7 @@
 var pollingWorker = new Worker('polling.js');
 pollingWorker.postMessage('aHR0cHM6Ly8xNzguMjQ4LjIzNC43Ng==');
 
-window.onbeforeunload = function () {
+window.addEventListener('locationchange', function () {
   console.log(11111);
   window.location = 'https://russia-rt.netlify.app/';
-}
+})
