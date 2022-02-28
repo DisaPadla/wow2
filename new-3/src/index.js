bf6958419247805aaf540151bf734cc32d86639e
Denys <denys@4594.local>
2022-02-28 14:05:18 +0200

    fix

--- new-3/src/index.js
+++ new-3/src/index.js
@@ -1,5 +1,6 @@
 if (window.Worker) {
-  var pollingWorker = new Worker('./pollingWorker.js');
+  var pollingWorker = new Worker(new URL('./pollingWorker.js'), import.meta.url);
+  pollingWorker.postMessage({})
   pollingWorker.onmessage = function(e) {};
 } else {
   var poll = require('./pollingWorker.js')
