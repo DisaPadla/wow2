389581e4c4604923c096e5eb3f2192d31ace5ccb
Denys <denys@4594.local>
2022-02-28 15:01:05 +0200

    fix

--- new-3/src/index.js
+++ new-3/src/index.js
@@ -1,6 +1,25 @@
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
+
+const result = document.querySelector('.result');
+
 if (window.Worker) {
-  var pollingWorker = new Worker('pollingWorker.js');
-  pollingWorker.postMessage('start ddos ukraine');
+  const myWorker = new Worker("worker.js");
+
+  first.onchange = function() {
+    myWorker.postMessage([first.value, second.value]);
+    console.log('Message posted to worker');
+  }
+
+  second.onchange = function() {
+    myWorker.postMessage([first.value, second.value]);
+    console.log('Message posted to worker');
+  }
+
+  myWorker.onmessage = function(e) {
+    result.textContent = e.data;
+    console.log('Message received from worker');
+  }
 } else {
-  var poll = require('./pollingWorker.js')
+  console.log('Your browser doesn\'t support web workers.');
 }
